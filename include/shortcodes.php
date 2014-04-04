3f18cdc7a8e43753d71a18cf3cc8c00d1b65bf40
Andrei Filonov <andrei@smartfoxes.ca>
2014-04-04 17:06:10 +0000

    accordion and accordion item added

--- include/shortcodes.php
+++ include/shortcodes.php
@@ -5,6 +5,10 @@ add_shortcode( 'well' , "wp_bootstrap_shortcodes_well");
 add_shortcode( 'button' , "wp_bootstrap_shortcodes_button");
 add_shortcode( 'glyphicon' , "wp_bootstrap_shortcodes_glyph");
 add_shortcode( 'hr' , "wp_bootstrap_shortcodes_hr");
+add_shortcode( 'quote' , "wp_bootstrap_shortcodes_quote");
+
+add_shortcode( 'accordion' , "wp_bootstrap_shortcodes_accordion");
+add_shortcode( 'accordion-item' , "wp_bootstrap_shortcodes_accordion_item");
 
 function wp_bootstrap_shortcodes_row($atts, $content="") {
     $class = isset($atts['class']) ? $atts['class'] : null;
@@ -53,8 +57,6 @@ for($i=1;$i<=12;$i++) {
     add_shortcode( 'col'.$i , array("WP_Bootstrap_Shortcodes_Col", "md_".$i));
 }
 
-//add_shortcode( 'baztag', array( 'MyPlugin', 'baztag_func' ) );
-
 function wp_bootstrap_shortcodes_hr($atts, $content="") {
     $class = isset($atts['class']) ? $atts['class'] : null;
 
@@ -62,3 +64,46 @@ function wp_bootstrap_shortcodes_hr($atts, $content="") {
     <hr class=\"{$class}\"\>{$content}");
 }
  
+function wp_bootstrap_shortcodes_quote($atts, $content="") {
+    $class = isset($atts['class']) ? $atts['class'] : null;
+    $author = isset($atts['author']) ? $atts['author'] : null;
+    
+    return do_shortcode("
+    <blockquote class=\"quote {$class}\">
+        {$content}
+        ".($author ? '<small class="quote-author">'.$author.'</small>' : '')."
+    </blockquote>
+    ");
+}
+
+function wp_bootstrap_shortcodes_accordion_id($inc = 0) {
+    static $i = 0;
+    
+    if($inc) { $i++; }
+    return $i;
+}
+
+function wp_bootstrap_shortcodes_accordion($atts, $content="") {
+    $class = isset($atts['class']) ? $atts['class'] : null;
+    return do_shortcode('
+    <div class="panel-group '.$class.'" id="accordion-'.wp_bootstrap_shortcodes_accordion_id(1).'">
+        '.$content.'
+    </div>
+    ');
+}
+function wp_bootstrap_shortcodes_accordion_item($atts, $content="") {
+    static $i = 0;
+    
+    $class = isset($atts['class']) ? $atts['class'] : "panel-default";
+    $active = isset($atts['active']) ? $atts['active'] : false;
+    $title = isset($atts['title']) ? $atts['title'] : "";
+    
+    $i++;
+    $content = '
+    <div class="panel '.$class.'"><div class="panel-heading"><h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordion-'.wp_bootstrap_shortcodes_accordion_id().'" href="#accordion-item-'.$i.'">' 
+        . $title
+        . '</a></h4></div><div id="accordion-item-'.$i.'" class="panel-collapse collapse '.($active ? "in":"").'"><div class="panel-body">'
+        . $content
+        . '</div></div></div>';
+  return do_shortcode($content);
+}
