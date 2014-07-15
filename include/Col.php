3b76d94c69f7248292c793fadc07bf697d6728f2
Andrei Filonov <andrei@smartfoxes.ca>
2014-07-15 05:49:54 +0000

    attributes fixes

--- include/Col.php
+++ include/Col.php
@@ -6,14 +6,19 @@ class WP_Bootstrap_Shortcodes_Col {
     //    return do_shortcode("Call to {$name} with args: <pre>" . print_r($args, true) . "</pre>");        
     }
     public  static function __callStatic ($name, $attribs) {
-        list($atgs,$content) = $attribs;
+        list($args,$content) = $attribs;
         list($screen,$size) = explode("_",$name);
         return do_shortcode(self::col($size,$screen,$args, $content));
     }
     
     
-    public static function col($size=4, $screen="md", $args, $content) {
-        $class = isset($atts['class']) ? $atts['class'] : null;
+    public static function col($size=4, $screen="sm", $args, $content) {
+        $class = isset($args['class']) ? $args['class'] : null;
+        foreach($args as $att) {
+            if(preg_match("/^offset(\d+)$/i",$att,$m)) {
+                $class .= " col-{$screen}-offset-" . $m[1];
+            }
+        }
         return "
         <div class=\"col-{$screen}-{$size} {$class}\">
             {$content}
